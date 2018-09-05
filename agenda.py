455691dc50abd7028f45bdb1edd1f93002fd12fb
rvarizano <42013709+rvarizano@users.noreply.github.com>
2018-09-05 19:11:02 -0300

    Atualização agenda.py

--- agenda.py
+++ agenda.py
@@ -2,21 +2,25 @@ from funcoes import *
 
 opt = 0
 contato = ""
+tel = ""
 while opt != 5:
     bemvindo()
     opt = int(input("Digite a opção desejada: "))
     print("\n")
 
-    if opt == 1:
-        adicionar()
-    elif opt == 2:
+    if opt == 1: #adicionar
+        contato = input("Insira o nome do contato que será adicionado: ")
+        tel = input("Insira o número de telefone do contato: ")
+        adicionar(contato, tel)
+    elif opt == 2: #listar
         listarR()
-    elif opt == 3:
-        deletar()
-    elif opt == 4:
+    elif opt == 3: #deletar
+        contato = input("Insira o nome do contato que será deletado: ")
+        deletar(contato)
+    elif opt == 4: #buscar
         contato = input("Insira o nome do contato que será buscado: ")
         encontrar(contato)
-    elif opt == 5:
+    elif opt == 5: #encerrar
         print("Encerrando aplicação.")
-    elif opt < 1 or opt > 5:
+    elif opt < 1 or opt > 5: #opção invalida
         falha()
